4ea37956ae77f95463ec3c766cde7ec0c740d5a3
Nagi2880 <alexanderamaral17@gmail.com>
2022-03-07 12:37:57 -0300

    Add env variable in mongodb url

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
+import config from './config'
 
  //Call a anonym function to start a await
 (async () => {
-    const db = await mongoose.connect('mongodb://localhost/AnimesongsAPI')
-    console.log(`The name of the Database is ${db.connection.name}`);
+    const db = await mongoose.connect(config.mongodbURL)
+    console.log(`Database connected to ${db.connection.name}`);
 })();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
 import app from './app'
 //Import the database file to start it
 import './database';
-
+//Import 
 app.listen(app.get('port'))
 console.log('server on port', app.get('port'))
