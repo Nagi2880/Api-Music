30d7723c35961012633e1eef02a0ee8ed9bcdc41
Nagi2880 <alexanderamaral17@gmail.com>
2022-03-07 18:42:54 -0300

    Added the first song on the DB

--- src/routes/music.routes.js
+++ src/routes/music.routes.js
@@ -1,8 +1,23 @@
-import {Router} from 'express'
+import {Router} from 'express';
+import Anime from '../models/Anime';
+
 
 const router = Router()
 
 router.get('/', (req, res) => {
     res.send('Musics')
 })
+
+router.post('/', async (req,res) =>{
+    const newAnime = new Anime({
+        animename: req.body.animename,
+        japanname: req.body.japanname,
+        director: req.body.director,
+        songs: req.body.songs,
+        })
+    const animeSaved = await newAnime.save();
+    res.json(animeSaved)
+
+})
+
 export default router;
