f7d05167be4b8bab7d22a56b91a24aee5b9d1a07
Nagi2880 <alexanderamaral17@gmail.com>
2022-03-09 17:30:51 -0300

    cors and morgan in the project

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 //Config from express
-import express from 'express'
-import morgan from 'morgan'
-import Musicroutes from './routes/music.routes'
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import Musicroutes from './routes/music.routes';
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 3000)
 //Middlewares
+app.use(cors)
 app.use(morgan('dev'))
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 // routes
 app.get('/', (req, res) => {
     res.json({ message: 'test' })
--- src/database.js
+++ src/database.js
@@ -3,6 +3,10 @@ import config from './config'
 
  //Call a anonym function to start a await
 (async () => {
-    const db = await mongoose.connect(config.mongodbURL)
-    console.log(`Database connected to ${db.connection.name}`);
+    try{
+        const db = await mongoose.connect(config.mongodbURL);
+        console.log(`Database connected to ${db.connection.name}`);
+    }catch (error){
+        console.error(error);
+    }
 })();
